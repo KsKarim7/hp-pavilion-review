5671506bf7311cdf5f6e16a99944d49a62e5c039
KsKarim <kskarim177@gmail.com>
2022-04-04 21:07:24 +0600

    home page almost done

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import "./Header.css"
 
 const Header = () => {
     return (
-        <nav>
-            <Link to="/home">HOME</Link>
-            <Link to="/reviews">REVIEWS</Link>
-            <Link to="/dashboard">DASHBOARD</Link>
-            <Link to="/blogs">BLOGS</Link>
-            <Link to="/about">ABOUT</Link>
-        </nav>
+        // <nav>
+        //     <Link to="/home">HOME</Link>
+        //     <Link to="/reviews">REVIEWS</Link>
+        //     <Link to="/dashboard">DASHBOARD</Link>
+        //     <Link to="/blogs">BLOGS</Link>
+        //     <Link to="/about">ABOUT</Link>
+        // </nav>
+        <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="/home">HP</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Link to="/home">HOME</Link>
+                    <Link to="/reviews">REVIEWS</Link>
+                    <Link to="/dashboard">DASHBOARD</Link>
+                    <Link to="/blogs">BLOGS</Link>
+                    <Link to="/about">ABOUT</Link>
+                </Nav>
+            </Container>
+        </Navbar>
     );
 };
 
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Button, Col, Container, Row } from 'react-bootstrap';
+import pavilion from '../../images/pavilion.webp'
+import './Home.css'
 
 const Home = () => {
     return (
         <div>
-            <h1>This is home page of the site</h1>
+            <Container className='bar'>
+                <div className='info'>
+                    <h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Perferendis ea enim nulla, eos tenetur non fugit ad molestiae doloremque? Officiis autem facilis consectetur? Illum architecto at odio non. Sequi, tenetur?</h4>
+                    <br />
+                    <Button variant='dark'>See Live Demo</Button>
+                </div>
+                <div>
+                    <img src={pavilion} alt="" />
+                </div>
+            </Container>
+            <div className='review'>
+                <h1>See all reviews here</h1>
+            </div>
         </div>
     );
 };
